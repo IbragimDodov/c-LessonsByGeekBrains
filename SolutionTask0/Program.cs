bd49d0079db115d6b067cefa1b23820cefea4613
IbragimDodov <utisey@gmail.com>
2022-07-13 22:29:29 +0300

    add solution for task 0

---
+++ SolutionTask0/Program.cs
@@ -0,0 +1,11 @@
+﻿string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+  int inputNumber = int.Parse(inputLine);
+
+  // int outNumber = inputNumber * inputNumber;
+  int outNumber = (int)Math.Pow(inputNumber, 2);
+
+  Console.WriteLine(outNumber);
+}
