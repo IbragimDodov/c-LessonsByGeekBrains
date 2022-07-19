a22b0804a08efca4d5c95e6e5b7b35ffe4f6e65c
IbragimDodov <utisey@gmail.com>
2022-07-19 20:42:56 +0300

    added homework solution for task 15

---
+++ SolutionTask15/Program.cs
@@ -0,0 +1,11 @@
+﻿int inputNumber = int.Parse(Console.ReadLine());
+
+
+if ((inputNumber == 6) || (inputNumber == 7))
+{
+  Console.WriteLine("Выходной день");
+}
+else
+{
+  Console.WriteLine("Рабочий день");
+}
