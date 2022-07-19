1bc14edccee6528b7ee816e7a8c8ddd557d7f473
IbragimDodov <utisey@gmail.com>
2022-07-19 18:19:54 +0300

    add solution for task 12

---
+++ SolutionTask12/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string? inputOne = Console.ReadLine();
+string? inputTwo = Console.ReadLine();
+
+if (inputOne != null && inputTwo != null)
+{
+  int inputNumberOne = int.Parse(inputOne);
+  int inputNumberTwo = int.Parse(inputTwo);
+
+  int div = inputNumberTwo % inputNumberOne;
+
+  if (div == 0)
+  {
+    Console.WriteLine("yes");
+  }
+  else
+  {
+    Console.WriteLine(div);
+  }
+}
