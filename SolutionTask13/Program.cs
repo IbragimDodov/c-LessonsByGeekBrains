d0256805144a9f4447afef85be0cf5657638d89f
IbragimDodov <utisey@gmail.com>
2022-07-19 20:28:32 +0300

    added homework solution for task 13

---
+++ SolutionTask13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+string? inputLine = Console.ReadLine();
+
+Char[] someArray = inputLine.ToCharArray();
+if (someArray.Length >= 3)
+{
+  Console.WriteLine(someArray[2]);
+}
+else
+{
+  Console.WriteLine("please write your number correct, there must have 3 number");
+}
