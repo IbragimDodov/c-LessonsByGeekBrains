27205341165a89218008c5dff81b25baf2cbd8ee
IbragimDodov <utisey@gmail.com>
2022-07-19 18:32:46 +0300

    add solution for task 14

---
+++ SolutionTask14/Program.cs
@@ -0,0 +1,32 @@
+﻿
+string reminderSearcher(string? str)
+{
+  if (str != null)
+  {
+    try
+    {
+      string answer;
+      int num = int.Parse(str);
+      if (((num % 7) == 0) && ((num % 23) == 0))
+      {
+        answer = "It`s multiple for 7 and 23";
+      }
+      else
+      {
+        answer = "It`s not multiple for 7 and 23";
+      }
+      return answer;
+    }
+    catch (Exception e)
+    {
+      return "Возникло исключение: " + e.Message;
+    }
+  }
+  return "";
+}
+
+Console.WriteLine("Input number: ");
+
+string? inputOne = Console.ReadLine();
+
+Console.WriteLine(reminderSearcher(inputOne));
