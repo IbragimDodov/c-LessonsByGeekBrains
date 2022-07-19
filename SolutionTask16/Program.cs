4d55762ae0eaa1c8c3cf690bf402b4b90bcdcd90
IbragimDodov <utisey@gmail.com>
2022-07-19 20:12:03 +0300

    add solution for task 16

---
+++ SolutionTask16/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+
+Console.WriteLine("Add your first number: ");
+string? inputFirst = Console.ReadLine();
+Console.WriteLine("Add your second number: ");
+string? inputSecond = Console.ReadLine();
+
+
+if ((inputFirst != null) && (inputSecond != null))
+{
+  int inputNumberFirst = int.Parse(inputFirst);
+  int inputNumberSecond = int.Parse(inputSecond);
+  int a = inputNumberFirst * inputNumberFirst;
+  int b = inputNumberSecond * inputNumberSecond;
+  if ((a == inputNumberSecond) || (b == inputNumberFirst))
+  {
+    Console.WriteLine("yes");
+  }
+  else
+  {
+    Console.WriteLine("sorry, try again");
+  }
+}
