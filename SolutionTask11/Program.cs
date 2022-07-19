8ba5c03b9f18031f256beafe099051e2b6b84fc6
IbragimDodov <utisey@gmail.com>
2022-07-19 18:08:59 +0300

    add solution for task 11

---
+++ SolutionTask11/Program.cs
@@ -0,0 +1,15 @@
+﻿
+void middleDigitCutter2()
+{
+  int number = new Random().Next(100, 1000);
+  Console.Write("The random number is: ");
+  Console.WriteLine(number);
+
+  int num1 = number / 100;
+  int num2 = number % 10;
+
+  Console.Write("The modified num is: ");
+  Console.WriteLine(num1 * 10 + num2);
+}
+
+middleDigitCutter2();
