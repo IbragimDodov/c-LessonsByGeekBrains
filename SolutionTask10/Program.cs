25d72bd9056b56e189aed4ca540ca013d78e8c01
IbragimDodov <utisey@gmail.com>
2022-07-19 20:21:11 +0300

    added homework solution for task 10

---
+++ SolutionTask10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+  int inputNumber = int.Parse(inputLine);
+  int resNum = (inputNumber % 100) / 10;
+  Console.WriteLine(resNum);
+}
