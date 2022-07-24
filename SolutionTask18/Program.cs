d6540284d172877e96cb11369f494ab8bb4f939d
IbragimDodov <utisey@gmail.com>
2022-07-24 11:03:25 +0300

    added solution for task 18

---
+++ SolutionTask18/Program.cs
@@ -0,0 +1,16 @@
+﻿void printAnswer(int number)
+{
+  if (number == 1) Console.WriteLine("Допустимо: х > 0, y > 0");
+  if (number == 2) Console.WriteLine("Допустимо: х < 0, y > 0");
+  if (number == 3) Console.WriteLine("Допустимо: х < 0, y < 0");
+  if (number == 4) Console.WriteLine("Допустимо: х > 0, y < 0");
+}
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+  int inputNumber = int.Parse(inputLine);
+
+  printAnswer(inputNumber);
+}
