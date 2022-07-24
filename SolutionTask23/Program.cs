6973dcd4c649b1bfc1ab91f35cbb83ae49b219e8
IbragimDodov <utisey@gmail.com>
2022-07-24 13:02:34 +0300

    added homework solution for task 23

---
+++ SolutionTask23/Program.cs
@@ -0,0 +1,20 @@
+﻿string? inputLineN = Console.ReadLine();
+
+if (inputLineN != null)
+{
+  int numberN = int.Parse(inputLineN);
+
+  string lineN = "";
+  string lineNNN = string.Empty;
+  int s = 1;
+
+  while (s <= numberN)
+  {
+    lineN = lineN + s + " ";
+    lineNNN = lineNNN + (s * s * s).ToString() + " ";
+    s++;
+  }
+
+  Console.WriteLine(lineN);
+  Console.WriteLine(lineNNN);
+}
