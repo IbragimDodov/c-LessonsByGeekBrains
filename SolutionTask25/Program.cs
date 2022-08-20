488d839ffa9766fc90da75f1947f3030dea67261
IbragimDodov <utisey@gmail.com>
2022-08-20 12:24:50 +0300

    add solution for task 25

---
+++ SolutionTask25/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число ");
+string? a = Console.ReadLine();
+Console.Write("Введите число ");
+string? b = Console.ReadLine();
+
+void Read()
+{
+  if (a != null && b != null)
+  {
+    int parseA = int.Parse(a);
+    int parseB = int.Parse(b);
+
+    int c = (int)Math.Pow(parseA, parseB);
+
+    Console.WriteLine(c);
+  }
+
+}
+
+Read();
