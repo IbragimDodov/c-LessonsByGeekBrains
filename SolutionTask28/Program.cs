de354351e19191eee7b3eb51566150a4cf802563
IbragimDodov <utisey@gmail.com>
2022-08-20 11:59:17 +0300

    add solution for task 28

---
+++ SolutionTask28/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число ");
+string? inputLine = Console.ReadLine();
+int inputNum = int.Parse(inputLine);
+
+Console.WriteLine(sumNums(inputNum));
+
+int sumNums(int num)
+{
+  int sum = 1;
+  for (int i = 1; i <= inputNum; i++)
+  {
+    sum *= i;
+  }
+
+  return sum;
+}
