4c2f008c51c550304975df15f71f1ec7981d2c4d
IbragimDodov <utisey@gmail.com>
2022-08-20 12:10:23 +0300

    add solution for task 30

---
+++ SolutionTask30/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Random numberSintezator = new Random();
+
+void VariantNaive()
+{
+  int i = 0;
+  Console.Write("[");
+  while (i < 8)
+  {
+    Console.Write(numberSintezator.Next(0, 2) + ",");
+    i++;
+  }
+  Console.Write("]");
+}
+
+VariantNaive();
