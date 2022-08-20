36f1371bbe0d116ebee730deb2718cb158e8de9e
IbragimDodov <utisey@gmail.com>
2022-08-20 12:33:35 +0300

    add solution for task 27

---
+++ SolutionTask27/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число ");
+string? a = Console.ReadLine();
+int b = 0;
+
+void Calculate()
+{
+  int AtoNum = int.Parse(a);
+
+  while (AtoNum > 0)
+  {
+    b = b + AtoNum % 10;
+    AtoNum = AtoNum / 10;
+  }
+
+  Console.WriteLine(b);
+}
+
+Calculate();
