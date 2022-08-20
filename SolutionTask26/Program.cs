0e39d1742176832e13f80c723b82f126973b1ca5
IbragimDodov <utisey@gmail.com>
2022-08-20 11:50:55 +0300

    add solution for task 26

---
+++ SolutionTask26/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+
+string inputLineNumber = Console.ReadLine() ?? "";
+int inputNumber = int.Parse(inputLineNumber);
+
+int VariantChar()
+{
+  // int numberLength = 0;
+  // char[] array = inputLineNumber.ToCharArray();
+  // numberLength = array.Length;
+
+  return inputLineNumber.ToCharArray().Length;
+}
+
+int VariantSimple()
+{
+  int numberLength = 0;
+  int digits = 1;
+  while (digits < inputNumber)
+  {
+    digits = digits * 10;
+    numberLength++;
+  }
+
+  return numberLength;
+}
+
+int VariantLog10()
+{
+  return (int)Math.Log10(inputNumber) + 1;
+}
+
+int result = 0;
+result = VariantChar();
+Console.WriteLine(result);
+
+result = VariantSimple();
+Console.WriteLine(result);
+
+result = VariantLog10();
+Console.WriteLine(result);
