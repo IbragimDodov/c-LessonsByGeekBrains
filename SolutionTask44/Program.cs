160be16acca21d1bd05fb939733b7d6a42b69974
IbragimDodov <utisey@gmail.com>
2022-09-14 18:42:04 +0300

    add solution for task 44

---
+++ SolutionTask44/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Write("Введите колиество чисел в ряде Фибоначчи: ");
+int decemalNumber = ReadDecemalNumber();
+PrintAnswer(Fibonnachi(decemalNumber));
+
+int ReadDecemalNumber()
+{
+  return int.Parse(Console.ReadLine() ?? "");
+}
+
+string Fibonnachi(int n)
+{
+  string answer = "";
+
+  if (n == 2)
+  {
+    answer = "0 1";
+  }
+  else if (n == 1)
+  {
+    answer = "0";
+  }
+  else
+  {
+    answer = "0 1";
+    int fn_1 = 1;
+    int fn_2 = 0;
+    int fn;
+    for (int i = 0; i < n - 2; i++)
+    {
+      fn = fn_1 + fn_2;
+      answer += " " + fn;
+      fn_2 = fn_1;
+      fn_1 = fn;
+    }
+  }
+  return answer;
+}
+void PrintAnswer(string answer)
+{
+  Console.WriteLine("Ряд Фибоначчи для заданного кол-ва чисел: " + answer);
+}
